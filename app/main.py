aa69581197821a34f71d78178fcac3cc07dba53f
Sayfullin Ruslan <ruslansaifullin91@gmail.com>
2025-07-17 10:19:55 +0500

    Добавленпроект

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import os
-from typing import Optional
+from typing import Optional, List
+
 
 from fastapi import FastAPI
 
@@ -17,5 +18,32 @@ def home_page():
     return {"message": "Привет, Ruslan!"}
 
 @app.get("/students")
-def get_all_students():
-    return json_to_dict_list(path_to_json)
+def get_all_students(course: Optional[int] = None) -> List:
+    students = json_to_dict_list(path_to_json)
+    if course is None:
+        return students
+    else:
+        return_list = []
+        for student in students:
+            if student["course"] == course:
+                return_list.append(student)
+        return return_list
+
+@app.get("/students/{course}")
+def get_all_students_course(course: int, major: Optional[str] = None, enrollment_year: Optional[int] = 2018) -> List:
+    students = json_to_dict_list(path_to_json)
+    filtered_students = []
+    for student in students:
+        if student["course"] == course:
+            filtered_students.append(student)
+
+        if major:
+            filtered_students = [student for student in filtered_students if student['major'].lower() == major.lower()]
+
+        if enrollment_year:
+            filtered_students = [student for student in filtered_students if student['enrollment_year'] == enrollment_year]
+    
+    return filtered_students
+
+
+
